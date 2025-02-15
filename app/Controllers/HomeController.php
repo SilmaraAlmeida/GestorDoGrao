1f3e8e087297ee5e359ccd759613896e73976e6b
SilmaraAlmeida <silmaraalmeida226@gmail.com>
2025-02-15 16:37:30 -0300

    -

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -8,14 +8,9 @@ class HomeController extends BaseController
     {
         $data = [
             'titleHome' => 'Gestor do Grão - Home',
-            'faviconPath' => '/public/assets/images/cafe-favicon',
-            // 'cssPathCards' => '/public/assets/css/cards',
-            // 'cssPathNavbar' => '/public/assets/css/navbar',
         ];
 
         helper(['url', 'img']);
-
-        // $cssPath = [FCPATH . 'public/assets/css/cards.css'];
 
         return view('partials/header', $data) . view('home') . view('partials/footer');
     }
