3e23979c801d4f1a216e2ae3a470e98eaf55b3c3
SilmaraAlmeida <silmaraalmeida226@gmail.com>
2025-03-05 14:09:28 -0300

    adjustments to some tables

--- app/Views/portalProdutor/cadastrarProduto.php
+++ app/Views/portalProdutor/cadastrarProduto.php
@@ -27,10 +27,6 @@
                     <input type="text" class="form-control" name="nome_produto" id="nome_produto" required>
                 </div>
                 <div class="form-group mt-4 mb-4">
-                    <label for="tipo_cafe">Tipo de Café</label>
-                    <input type="text" class="form-control" name="tipo_cafe" id="tipo_cafe" required>
-                </div>
-                <div class="form-group mt-4 mb-4">
                     <label for="categoria">Categoria</label>
                     <input type="text" class="form-control" name="categoria" id="categoria" required>
                 </div>
@@ -38,16 +34,12 @@
                     <label for="quantidade_estoque">Quantidade em Estoque</label>
                     <input type="text" class="form-control" name="quantidade_estoque" id="quantidade_estoque" required>
                 </div>
+            </div>
+            
+            <div class="col-md-6">
                 <div class="form-group mt-4 mb-4">
                     <label for="preco_compra">Preço de Compra</label>
                     <input id="money" type="text" class="form-control" name="preco_compra" id="preco_compra" required>
-                </div>
-            </div>
-    
-            <div class="col-md-6">
-                <div class="form-group mt-4 mb-4">
-                    <label for="preco_venda">Preço de Venda</label>
-                    <input id="money2" type="text" class="form-control" name="preco_venda" id="preco_venda" required>
                 </div>
                 <div class="form-group mt-4 mb-4">
                     <label for="fornecedor">Fornecedor</label>
@@ -56,10 +48,6 @@
                 <div class="form-group mt-4 mb-4">
                     <label for="data_compra">Data de Compra</label>
                     <input type="text" class="form-control" name="data_compra" id="data_compra" required>
-                </div>
-                <div class="form-group mt-4 mb-4">
-                    <label for="notas_adicionais">Notas Adicionais</label>
-                    <input type="text" class="form-control" name="notas_adicionais" id="notas_adicionais" required>
                 </div>
             </div>
         </div>
@@ -77,6 +65,7 @@
 <script>
     $('#money').mask("#.##0,00", {reverse: true});
     $('#money2').mask("#.##0,00", {reverse: true});
+    $('#data_compra').mask('00/00/0000');
 </script>
 
 
--- app/Views/portalProdutor/estoqueAtual.php
+++ app/Views/portalProdutor/estoqueAtual.php
@@ -10,28 +10,24 @@
         <thead>
             <tr>
                 <th>Nome do Produto</th>
-                <th>Tipo de Café</th>
                 <th>Categoria</th>
                 <th>Quantidade em Estoque</th>
                 <th>Preco de Compra</th>
                 <th>Preco de Venda</th>
                 <th>Fornecedor</th>
                 <th>Data de Compra</th>
-                <th>Notas Adicionais</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($dados as $dado): ?>
                 <tr>
                     <td><?= $dado['nome_produto'] ?></td>
-                    <td><?= $dado['tipo_cafe'] ?></td>
                     <td><?= $dado['categoria'] ?></td>
                     <td><?= $dado['quantidade_estoque'] ?></td>
                     <td><?= number_format($dado['preco_compra'], 2, ',', '.') ?></td>
                     <td><?= number_format($dado['preco_venda'], 2, ',', '.') ?></td>
                     <td><?= $dado['fornecedor'] ?></td>
                     <td><?= $dado['data_compra'] ?></td>
-                    <td><?= $dado['notas_adicionais'] ?></td>
                 </tr>
             <?php endforeach ?>
         </tbody>
