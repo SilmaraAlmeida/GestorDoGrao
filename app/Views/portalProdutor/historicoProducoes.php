1a6254d9d37a7400a01329bf2290b93b98270cb9
SilmaraAlmeida <silmaraalmeida226@gmail.com>
2025-03-06 12:58:32 -0300

    HTML skeleton for modified productions change history

--- app/Views/portalProdutor/historicoProducoes.php
+++ app/Views/portalProdutor/historicoProducoes.php
@@ -1,53 +1,41 @@
 <?php echo $this->extend('portalProdutor/homePortal') ?>
 <?php echo $this->section('content') ?>
-    <h3 class="text text-center mt-5 mb-5">Histórico de produções</h3>
+<h3 class="text text-center mt-5 mb-5">Histórico de produções</h3>
 
 <div class="bg-light">
     <div class="container mt-5 mb-5">
-        <?php if (!empty($historico)) : ?>
-            <table class="table table-bordered table-striped" id="tabela">
-                <thead>
-                    <tr>
-                        <th>Ação</th>
-                        <th>Produção</th>
-                        <th>Campo Alterado (se aplicável)</th>
-                        <th>Usuário</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($historico as $item): ?>
-                        <tr>
-                            <td>
+        <?php
+            if (!empty($historico)) {
+                foreach ($historico as $item) {
+                    switch ($item['acao']) {
+                        case 'INSERT':
+                            ?> 
+                                <p>O usuário '<?= session()->get('user')->nome ?>' <span class="text-success">criou</span> a produção '<?= $item['nome_producao_novo'] ?>'</p>
+                                <p>Data da ação: <?= $item['alterado_em'] ?></p>
+                                <hr>
                                 <?php
-                                    switch ($item['tipo_acao']) {
-                                        case 'INSERT':
-                                            echo 'Registro';
-                                            break;
-                                        case 'UPDATE':
-                                            echo 'Alterado';
-                                            break;
-                                        case 'DELETE':
-                                            echo 'Deletado';
-                                            break;
-                                    }
-                                ?>
-                            </td>
-                            <td><?= $item['nome_producao'] ?></td>
-                            <td>
-                                <?php if ($item['tipo_acao'] == 'UPDATE'): ?>
-                                    <?= $item['nome_coluna'] ?>
-                                <?php else: ?>
-                                    N/A
-                                <?php endif; ?>
-                            </td>
-                            <td><?= session()->get('user')->nome ?></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        <?php else: ?>
-            <h4 class="text-center mb-4">Nenhuma alteração registrada.</h4>
-        <?php endif; ?>
+                        break;
+                        case 'UPDATE':
+                            ?> 
+                                <p>O usuário '<?= session()->get('user')->nome ?>' <span class="text-primary">atualizou</span> a produção '<?= $item['nome_producao_novo'] ?>'</p>
+                                <p>Data da ação: <?= $item['alterado_em'] ?></p>
+                                <p>Registro Modificado: </p>
+                                <hr>
+                                <?php
+                            break;
+                        case 'DELETE':
+                                ?> 
+                                <p>O usuário '<?= session()->get('user')->nome ?>' <span class="text-danger">deletou</span> a produção '<?= $item['nome_producao_antigo'] ?>'</p>
+                                <p>Data da ação: <?= $item['alterado_em'] ?></p>
+                                <hr>
+                            <?php
+                        break;
+                    }
+                }
+            } else {
+                echo 'Sem alterações registradas';
+            }
+        ?>
     </div>
 </div>
 
