42778dd1913bb3b8f2f24ddddd981ac919f55641
SilmaraAlmeida <silmaraalmeida226@gmail.com>
2025-03-07 09:56:13 -0300

    updated

--- app/Views/portalProdutor/formEditarProducoes.php
+++ app/Views/portalProdutor/formEditarProducoes.php
@@ -16,7 +16,7 @@
         <span class="text text-success"><?= session()->getFlashdata('success') ?></span>
     <?php endif ?>
 </div>
-        
+
 <div class="container">
     <form action="" method="post">
         <div class="row">
@@ -24,7 +24,6 @@
                 <div class="form-group mt-4 mb-4">
                     <label for="nome_producao">Nome da Produção</label>
                     <input type="text" class="form-control" name="nome_producao" id="nome_producao" placeholder="Ex.: José Silva" value="<?php echo isset($producoes['nome_producao']) ? $producoes['nome_producao'] : '' ?>" required>
-    
                 </div>
                 <div class="form-group mt-4 mb-4">
                     <label for="estado">Estado</label>
@@ -39,7 +38,7 @@
                     <input type="tel" class="form-control" name="telefone" id="telefone" placeholder="Ex.: (XX) XXXXX-XXXX" value="<?php echo isset($producoes['telefone']) ? $producoes['telefone'] : '' ?>" required>
                 </div>
             </div>
-    
+
             <div class="col-md-6">
                 <div class="form-group mt-4 mb-4">
                     <label for="area_plantada">Área Plantada (em hectares)</label>
@@ -52,12 +51,13 @@
                 <div class="form-group mt-4 mb-4">
                     <label for="metodos_cultivo">Metodos de Cultivo</label>
                     <select name="metodos_cultivo" class="form-control" id="metodos_cultivo">
-                        <option value="Cultivo Tradicional">Cultivo Tradicional</option>
-                        <option value="Agro Floresta">Agro Floresta</option>
-                        <option value="Sistema de Sombras">Sistema de Sombras</option>
-                        <option value="Cultivo Orgânico">Cultivo Orgânico</option>
+                        <?php $metodoCultivo = $producoes['metodos_cultivo'] ?>
+                        <?php foreach ($metodos_cultivo as $metodo_cultivo): ?>
+                            <option value="<?= $metodo_cultivo['nome_metodo'] ?>" <?= $metodoCultivo == $metodo_cultivo['nome_metodo'] ? 'selected' : '' ?>><?= $metodo_cultivo['nome_metodo'] ?></option>
+                        <?php endforeach ?>
                     </select>
                 </div>
+
                 <div class="form-group mt-4 mb-4">
                     <label for="producao_safra">Quantidade média produzido na ultima safra</label>
                     <input type="text" class="form-control" name="producao_safra" id="producao_safra" placeholder="Ex.: XXXX kg" value="<?php echo isset($producoes['producao_safra']) ? $producoes['producao_safra'] : '' ?>" required>
@@ -70,10 +70,10 @@
             <a href="<?= url_to('form_producao') ?>" class="btn btn-danger">Cancelar</a>
         </div>
     </form>
-    
+
     <div class="container text-center duvidas">
         <h4 class="mb-5">Em Caso de Dúvidas:</h4>
-    
+
         <p><strong>Cultivo Tradicional</strong> -> Plantio em linhas, manejo convencional do solo, irrigação e controle de pragas.</p>
         <p><strong>Agro Floresta</strong> -> Integra o cultivo de café com outras plantas, como árvores frutíferas e madeireiras.</p>
         <p><strong>Sistema de Sombras</strong> -> Utiliza árvores para fornecer sombra aos cafeeiros, moderando a temperatura e protegendo do sol direto.</p>
