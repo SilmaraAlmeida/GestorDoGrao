8840e85cc37f227d04de6177da2258be17efc7e2
SilmaraAlmeida <silmaraalmeida226@gmail.com>
2025-03-11 13:07:30 -0300

    corrected indentation

--- public/js/monitoramento.js
+++ public/js/monitoramento.js
@@ -1,25 +1,25 @@
-
 // CHART JS
 new Chart(document.getElementById("bar-chart-grouped"), {
     type: 'bar',
     data: {
-      labels: ["Janeiro", "Fevereiro", "Março", "Abril"],
-      datasets: [
-        {
-          label: "Faturamento",
-          backgroundColor: "#2d6a4f",
-          data: [133,221,783,2478]
-        }, {
-          label: "Perda",
-          backgroundColor: "#40916c",
-          data: [408,547,675,734]
-        }
-      ]
+        labels: ["Janeiro", "Fevereiro", "Março", "Abril"],
+        datasets: [
+            {
+                label: "Faturamento",
+                backgroundColor: "#2d6a4f",
+                data: [133, 221, 783, 2478]
+            },
+            {
+                label: "Perda",
+                backgroundColor: "#40916c",
+                data: [408, 547, 675, 734]
+            }
+        ]
     },
     options: {
-      title: {
-        display: true,
-        text: 'Population growth (millions)'
-      }
+        title: {
+            display: true,
+            text: 'Population growth (millions)'
+        }
     }
 });
