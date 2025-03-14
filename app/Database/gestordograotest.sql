ed62be4051b626db70354ba03f18fb1e6da60738
SilmaraAlmeida <silmaraalmeida226@gmail.com>
2025-03-14 12:51:09 -0300

    database updated

--- app/Database/gestordograotest.sql
+++ app/Database/gestordograotest.sql
@@ -50,24 +50,22 @@ CREATE TABLE IF NOT EXISTS vendas (
 )ENGINE=InnoDB DEFAULT CHARSET = UTF8;
 
 CREATE TABLE IF NOT EXISTS controle_estoque (
-	id INT NOT NULL,
+	id INT NOT NULL AUTO_INCREMENT,
 	id_usuario INT NOT NULL,
 	nome_produto VARCHAR(100) NOT NULL,
-	tipo_cafe VARCHAR(100) NOT NULL,
 	categoria VARCHAR(100) NOT NULL,
 	quantidade_estoque INT NOT NULL,
 	preco_compra DECIMAL(10, 2) NOT NULL,
 	preco_venda DECIMAL(10, 2) NOT NULL,
-	fornecedor DECIMAL(10, 2) NOT NULL,
+	fornecedor VARCHAR(50) NOT NULL,
 	data_compra DATE NOT NULL,
-	notas_adicionais TEXT,
 	PRIMARY KEY (id),
 	KEY controle_estoque_usuario_id_foreign (id_usuario),
 	CONSTRAINT controle_estoque_usuario_id_foreign FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 )ENGINE=InnoDB DEFAULT CHARSET = UTF8;
 
 CREATE TABLE IF NOT EXISTS custos_lavoura (
-	id INT NOT NULL,
+	id INT NOT NULL AUTO_INCREMENT,
 	id_usuario INT NOT NULL,
 	data_registro TIMESTAMP NOT NULL,
 	custo_insumo DECIMAL(10, 2) NOT NULL,
@@ -95,19 +93,17 @@ CREATE TABLE IF NOT EXISTS metodos_cultivo (
 
 CREATE TABLE IF NOT EXISTS producao_cafe (
     id INT NOT NULL AUTO_INCREMENT,
-    id_usuario INT NOT NULL,
     nome_producao VARCHAR(100) NOT NULL,
     estado VARCHAR(100) NOT NULL,
     cidade VARCHAR(100) NOT NULL,
     telefone VARCHAR(15) NOT NULL,
     area_plantada DECIMAL(10, 2) NOT NULL,
-    id_variedade_cafe INT NOT NULL,
-    id_metodos_cultivo INT NOT NULL,
+    variedade_cafe VARCHAR(50) NOT NULL,
+    metodos_cultivo VARCHAR(50) NOT NULL,
     producao_safra INT NOT NULL,
+    id_usuario INT NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT producao_usuario_id_foreign FOREIGN KEY (id_usuario) REFERENCES usuario (id),
-    CONSTRAINT variedade_cafe_id_foreign FOREIGN KEY (id_variedade_cafe) REFERENCES variedade_cafe (id),
-    CONSTRAINT metodos_cultivo_id_foreign FOREIGN KEY (id_metodos_cultivo) REFERENCES metodos_cultivo (id)
+    CONSTRAINT producao_usuario_id_foreign FOREIGN KEY (id_usuario) REFERENCES usuario (id)
 )ENGINE=InnoDB DEFAULT CHARSET = UTF8;
 
 
